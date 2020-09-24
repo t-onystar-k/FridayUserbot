3fe6581fb9bf8159fd2c7a1e4c3da88a7be974ec
Restyled.io <commits@restyled.io>
2020-09-24 11:20:38 +0000

    Restyled by black

--- userbot/plugins/carbon.py
+++ userbot/plugins/carbon.py
@@ -30,7 +30,14 @@ from googleapiclient.errors import HttpError
 from googletrans import LANGUAGES, Translator
 from gtts import gTTS
 from emoji import get_emoji_regexp
-from userbot import CMD_HELP, BOTLOG, BOTLOG_CHATID, YOUTUBE_API_KEY, CHROME_DRIVER, GOOGLE_CHROME_BIN
+from userbot import (
+    CMD_HELP,
+    BOTLOG,
+    BOTLOG_CHATID,
+    YOUTUBE_API_KEY,
+    CHROME_DRIVER,
+    GOOGLE_CHROME_BIN,
+)
 from userbot.utils import register
 
 
@@ -44,7 +51,7 @@ async def carbon_api(e):
 
         """ A Wrapper for carbon.now.sh """
         await e.edit("`Processing..`")
-        CARBON = 'https://carbon.now.sh/?l={lang}&code={code}'
+        CARBON = "https://carbon.now.sh/?l={lang}&code={code}"
         global CARBONLANG
         textx = await e.get_reply_message()
         pcode = e.text
@@ -68,51 +75,56 @@ async def carbon_api(e):
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-gpu")
-        prefs = {'download.default_directory': './'}
-        chrome_options.add_experimental_option('prefs', prefs)
-        driver = webdriver.Chrome(
-            executable_path=CHROME_DRIVER, options=chrome_options)
+        prefs = {"download.default_directory": "./"}
+        chrome_options.add_experimental_option("prefs", prefs)
+        driver = webdriver.Chrome(executable_path=CHROME_DRIVER, options=chrome_options)
         driver.get(url)
         await e.edit("`Be Patient...\n50%`")
-        download_path = './'
+        download_path = "./"
         driver.command_executor._commands["send_command"] = (
-            "POST", '/session/$sessionId/chromium/send_command')
-        params = {'cmd': 'Page.setDownloadBehavior', 'params': {
-            'behavior': 'allow', 'downloadPath': download_path}}
+            "POST",
+            "/session/$sessionId/chromium/send_command",
+        )
+        params = {
+            "cmd": "Page.setDownloadBehavior",
+            "params": {"behavior": "allow", "downloadPath": download_path},
+        }
         command_result = driver.execute("send_command", params)
         driver.find_element_by_xpath(
-            '/html/body/div[1]/main/div[3]/div[2]/div[1]/div[1]/div/span[2]').click()
+            "/html/body/div[1]/main/div[3]/div[2]/div[1]/div[1]/div/span[2]"
+        ).click()
         if skeme != None:
             k_skeme = driver.find_element_by_xpath(
-                '/html/body/div[1]/main/div[3]/div[2]/div[1]/div[1]/div/span[2]/input')
+                "/html/body/div[1]/main/div[3]/div[2]/div[1]/div[1]/div/span[2]/input"
+            )
             k_skeme.send_keys(skeme)
             k_skeme.send_keys(Keys.DOWN)
             k_skeme.send_keys(Keys.ENTER)
         else:
             color_scheme = str(random.randint(1, 29))
-            driver.find_element_by_id(
-                ("downshift-0-item-" + color_scheme)).click()
+            driver.find_element_by_id(("downshift-0-item-" + color_scheme)).click()
         driver.find_element_by_id("export-menu").click()
         driver.find_element_by_xpath("//button[contains(text(),'4x')]").click()
-        driver.find_element_by_xpath(
-            "//button[contains(text(),'PNG')]").click()
+        driver.find_element_by_xpath("//button[contains(text(),'PNG')]").click()
         await e.edit("`Processing..\n75%`")
         # Waiting for downloading
         sleep(2.5)
         color_name = driver.find_element_by_xpath(
-            '/html/body/div[1]/main/div[3]/div[2]/div[1]/div[1]/div/span[2]/input').get_attribute('value')
+            "/html/body/div[1]/main/div[3]/div[2]/div[1]/div[1]/div/span[2]/input"
+        ).get_attribute("value")
         await e.edit("`Done Dana Done...\n100%`")
-        file = './carbon.png'
+        file = "./carbon.png"
         await e.edit("`Uploading..`")
         await e.client.send_file(
             e.chat_id,
             file,
             caption="<< `Here's your carbon!` \n **Carbonised Using** [ຊ໐hrē🧚🏻‍♀️](@zamaneh_z)>>\n**Colour Scheme: **`{}`".format(
-                color_name),
+                color_name
+            ),
             force_document=True,
             reply_to=e.message.reply_to_msg_id,
         )
-        os.remove('./carbon.png')
+        os.remove("./carbon.png")
         driver.quit()
         # Removing carbon.png after uploading
         await e.delete()  # Deleting msg
